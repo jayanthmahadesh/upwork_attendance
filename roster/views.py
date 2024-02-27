551ff5045c5565d7d2c25c6a5a5a943bd4fbb546
Sachin <sachnaror@gmail.com>
2024-02-27 17:51:35 +0530

    typing commit message is most boring part

--- roster/views.py
+++ roster/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
+
+
+def custom_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('/success-url/')
+        else:
+            # Return an 'invalid login' error message.
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
+    else:
+        return render(request, 'login.html')
