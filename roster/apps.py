f95e0cefe452785cbd55a0bc1e30e4b425b2104f
Sachin <sachnaror@gmail.com>
2024-02-27 17:31:49 +0530

    typing commit message is most boring part

--- roster/apps.py
+++ roster/apps.py
@@ -1,6 +1,24 @@
+from django.contrib.auth.models import Group
+from django.db import migrations
 from django.apps import AppConfig
 
 
 class RosterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'roster'
+
+
+def create_groups(apps, schema_editor):
+    Group.objects.get_or_create(name='Manager')
+    Group.objects.get_or_create(name='Staff')
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('roster', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_groups),
+    ]
