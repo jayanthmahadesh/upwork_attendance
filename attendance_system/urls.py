3b133e0cabb804d2372ea0ca4f564653c723f143
Sachin <sachnaror@gmail.com>
2024-02-27 18:46:00 +0530

    typing commit message is most boring part

--- attendance_system/urls.py
+++ attendance_system/urls.py
@@ -2,14 +2,15 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from .views import (roster_create_view, roster_delete_view, roster_list_view,
-                    roster_update_view)
+from .views import (mark_attendance, roster_create_view, roster_delete_view,
+                    roster_list_view, roster_update_view)
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', include('django.contrib.auth.urls')),
     path('roster/', roster_list_view, name='roster_list_url'),
     path('roster/add/', roster_create_view, name='roster_create_url'),
     path('roster/<int:id>/edit/', roster_update_view, name='roster_update_url'),
     path('roster/<int:id>/delete/', roster_delete_view, name='roster_delete_url'),
+    path('roaster/mark_attendance/', mark_attendance, name='mark_attendance'),
     # Your other urls
 ]
